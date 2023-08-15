7f4cce9853a75cf14f5a646e2f14e82a123a11e6
Ashad Naushad <mohda170602@gmail.com>
2023-08-16 01:43:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/healthx/HealthXApplication.kt
@@ -0,0 +1,5 @@
+package com.example.healthx
+
+import android.app.Application
+
+class HealthXApplication: Application()
---
+++ app/src/main/java/com/example/healthx/ui/activities/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.healthx.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.healthx.R
+import com.example.healthx.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+
+
+}
